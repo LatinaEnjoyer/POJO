2c5a600c7ed93bc746888004de79bb8891923025
LatinaEnjoyer <nioe1000@stud.ek.dk>
2026-04-03 22:21:58 +0200

    Clicker w/ javascript

--- src/Clicker/logic.js
+++ src/Clicker/logic.js
@@ -1,13 +1,11 @@
 let button = document.getElementById("btn");
 let count = 0;
 let counterText = document.getElementById("counter");
-let clickSound = new Audio("dry-fart.mp3");
 
 button.addEventListener("click", function () {
     console.log("click");
     count++;
     counterText.textContent = count;
-    clickSound.play();
 });
 
 
